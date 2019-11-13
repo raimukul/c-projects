1844eb26fedf09cc249019fc6c020db3afb9f6f9
raimukul <47410631+raimukul@users.noreply.github.com>
2019-11-13 11:54:06 +0530

    Update circle rangoli.c

--- circle rangoli.c
+++ circle rangoli.c
@@ -8,26 +8,18 @@ void rainbow()
     int x, y, i, j;
     initgraph(&gdriver,&gmode,"C:\\Turboc3\\BGI");
 
-    x = 0; //getmaxx() / 2;//finding centre x-ordinate of screen
-    y = 0; // getmaxy() / 2;//finding centre y-ordinate of screen
+    x = 0; 
+    y = 0; 
 
     for (i=50; i<300; i++)
     {
-	// delay function under dos.h for holding the
-	// function for some time
 	delay(100);
-
-	// selecting  color for making of rainbow
 	setcolor(i/10);
 
-	// making of arc with fixed centre and increasing radius
 	circle(x+i, y+i,80);
     }
 }
 
-
-
-// driver program
 void main()
 {
     rainbow();
